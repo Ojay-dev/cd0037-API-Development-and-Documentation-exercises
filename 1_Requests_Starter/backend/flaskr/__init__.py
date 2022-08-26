5b2b15a5217f18a7d259293669fc62ab4c369915
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2022-08-26 17:39:57 +0100

    (Feat): implement errorhandlers

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -20,13 +20,6 @@ def paginate_books(selection):
     return current_books
 
 
-# @TODO: General Instructions
-#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there.
-#     If you do not update the endpoints, the lab will not work - of no fault of your API code!
-#   - Make sure for each route that you're thinking through when to abort and with which kind of error
-#   - If you change any of the response body keys, make sure you update the frontend to correspond.
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -44,12 +37,6 @@ def create_app(test_config=None):
         )
         return response
 
-    # @TODO: Write a route that retrivies all books, paginated.
-    #         You can use the constant above to paginate by eight books.
-    #         If you decide to change the number of books per page,
-    #         update the frontend to handle additional books in the styling and pagination
-    #         Response body keys: 'success', 'books' and 'total_books'
-    # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
     @app.route("/books")
     def retrieve_books():
         selection = Book.query.order_by("id").all()
@@ -66,11 +53,6 @@ def create_app(test_config=None):
             }
         )
 
-    # @TODO: Write a route that will update a single book's rating.
-    #         It should only be able to update the rating, not the entire representation
-    #         and should follow API design principles regarding method and route.
-    #         Response body keys: 'success'
-    # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
     @app.route("/books/<int:book_id>", methods=["PATCH"])
     def update_book(book_id):
         body = request.get_json()
@@ -91,10 +73,6 @@ def create_app(test_config=None):
         except:
             abort(400)
 
-    # @TODO: Write a route that will delete a single book.
-    #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
-    #        Response body keys: 'success', 'books' and 'total_books'
-    # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
     @app.route("/books/<int:book_id>/delete", methods=["DELETE"])
     def delete_book(book_id):
         try:
@@ -119,10 +97,6 @@ def create_app(test_config=None):
         except:
             abort(422)
 
-    # @TODO: Write a route that create a new book.
-    #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
-    # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
-    #       Your new book should show up immediately after you submit it at the end of the page.
     @app.route("/books", methods=["POST"])
     def create_book():
         body = request.get_json()
@@ -149,5 +123,30 @@ def create_app(test_config=None):
 
         except:
             abort(422)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 405, "message": "method not allowed"}),
+            405,
+        )
 
     return app
